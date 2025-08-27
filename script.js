e675fe53e518f44406885af56791bcb3a2eacfc9
ohiofi <20hz20khz@gmail.com>
2025-08-27 11:33:27 -0400

    Update script.js

--- script.js
+++ script.js
@@ -98,6 +98,7 @@ const imageFilenames = [
     "hubgdad (14).png",
     "hubgdad (15).png",
     "hubgdad (16).png",
+    "hubgdad (17).png",
     "aicc (1).png",
     "aicc (2).png",
     "aicc (3).png",
